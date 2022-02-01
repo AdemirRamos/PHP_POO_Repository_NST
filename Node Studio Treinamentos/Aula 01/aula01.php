0209f7d653c164c7d7e418b0a8dd1b1f6ed0ffa0
AdemirRamos <ademirramos634@gmail.com>
2022-02-01 13:47:23 -0300

    Adição da Aula 01

---
+++ Node Studio Treinamentos/Aula 01/aula01.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 01 - Introdução</title>
+
+</head>
+
+<body>
+    
+    <?php
+
+        //Por quê devemos usar PHP POO? Porque esta é a maneira mais evoluída de trabalhar com a lingaguem.
+        //Esse é o paradigma que mais se aproxima do mundo real.
+
+    ?>
+
+</body>
+
+</html>
