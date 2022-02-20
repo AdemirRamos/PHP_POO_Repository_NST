a86979491dbd7b4233acde264d0a9aaf4e0c59b1
AdemirRamos <ademirramos634@gmail.com>
2022-02-20 09:20:12 -0300

    Adição da Aula 23

---
+++ Node Studio Treinamentos/Aula 23/aula23.php
@@ -0,0 +1,31 @@
+<?php
+
+    //Aula 23 - Composer install
+
+    //Instalando as bibliotecas/dependências manualmente:
+
+    //Dentro da pasta onde está o seu projeto, crie um arquivo novo com o nome "composer.json";
+    //Em seguida, faça isto (desconsidere os numerais):
+
+    /*
+
+        {
+            1ª: "require": {
+                2ª: "cocur/slugify" 3ª: "3.2" 3.1ª: ,
+            }
+        }
+    
+    */
+
+    //1ª: escreve "require";
+    //2ª: digite o nome da biblioteca entre aspas;
+    //3ª: digite a versão da biblioteca, entre aspas (após dois pontos);
+    //3.1ª: caso se tenha mais de uma dependência, basta, após a primeira, colocar uma vírgula e digitar a próxima (e assim por diante);
+    //4ª: dentro do "cmd", digite "composer install" (sem aspas);
+    //5ª: dentro do arquivo do projeto, digite (desconsiderar o ponto final): require_once 'vendor/autoload.php'.
+
+    //P. S.: "autoload", por enquanto, somente carrega as classes instaladas através do composer (classes da pasta "vendor");
+
+    //6ª: Use a biblioteca baixada. Na página do "Packagist", há instruções de uso na página do pacote baixado.
+    
+?>
