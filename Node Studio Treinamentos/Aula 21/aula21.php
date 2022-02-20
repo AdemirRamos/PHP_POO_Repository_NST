23ae0e47a103890aeaa64f5dfc39c5482df96fd7
AdemirRamos <ademirramos634@gmail.com>
2022-02-20 08:43:36 -0300

    Atualização da Aula 21

--- Node Studio Treinamentos/Aula 21/aula21.php
+++ Node Studio Treinamentos/Aula 21/aula21.php
@@ -1,5 +1,15 @@
 <?php
 
-    //Aula 21 - Variáveis de Ambiente e Servidor Embutido.
+    //Aula 21 - Variáveis de Ambiente e Servidor Embutido
+
+    echo 'Olá, mundo!';
+
+    //O comando acima (ou quaisquer outros que, eventualmente, sejam escritos) pode ser visualizado através do "prompt" de comando.
+
+    //Dicas:
+
+    //Para rodar um arquivo no "prompt" de comando, usamos o comando "php (nome do arquivo sem parênteses)".
+    //Para iniciar o servidor embutido do PHP através do "prompt" de comando, usamos o comando: "php -S localhost:(número da porta);
+    //O número da porta é facultativo. Recomanda-se a porta "80"; caso essa esteja ocupada, se pode usar "8080".
 
 ?>
