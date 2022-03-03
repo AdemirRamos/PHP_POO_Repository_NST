7a8e41098dea3edf13a125ab2ed66b8bffdeb601
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-03-02 22:45:01 -0300

    Atualização da Aula 01
    Apenas foram feitas correções semânticas na 5ª linha do documento.

--- Node Studio Treinamentos/Aula 01/aula01.php
+++ Node Studio Treinamentos/Aula 01/aula01.php
@@ -2,7 +2,7 @@
 
     //Aula 01 - Introdução
     
-    //Por que devemos usar PHP POO? Porque esta é a maneira mais evoluída de trabalhar com a linguagem.
+    //Por que devemos usar PHP POO? Porque esta é a maneira mais evoluída de se trabalhar com a linguagem.
     //Esse é o paradigma que mais se aproxima do mundo real.
 
 ?>
