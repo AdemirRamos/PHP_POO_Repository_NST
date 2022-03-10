15fc25f166e1c30aa7712115b4a26da4210cfa90
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-03-10 16:30:50 -0300

    Atualização da Aula 22
    Apenas foram feitas pequenas correções semânticas ao longo do documento.

--- Node Studio Treinamentos/Aula 22/aula22.php
+++ Node Studio Treinamentos/Aula 22/aula22.php
@@ -2,7 +2,7 @@
 
     //Aula 22 - Composer init, Require e Update
 
-    //Na pasta "vendor", temos todos os paotes e dependências de terceiros.
+    //Na pasta "vendor", temos todos os pacotes e dependências de terceiros.
 
     //"autoload" faz o carregamento automático e dinâmico destes pacotes.
 
@@ -11,7 +11,7 @@
     //"composer.lock" contem todas as especificações do nosso pacote.
 
     //Para baixar um novo pacote, dentro do "prompt" de comando, digite (sem aspas): "composer require (nome do pacote sem parênteses)".
-    //Após o download do novo pacote, o seu arquivo "composer.json" vai ser atualizado e mostratá o novo pacote baixado.
+    //Após o download do novo pacote, o seu arquivo "composer.json" vai ser atualizado e mostrará o novo pacote baixado.
     //Em "vendor", aparecerá uma nova pasta proveniente do pacote baixado.
     
     //Para remover o pacote, basta remover, do "composer.json", a linha correspondente ao pacote que deve ser desinstalado.
